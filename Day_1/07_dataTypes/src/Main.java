f2ee08268adbacdb93bf151cdaea1482696e1931
yucelenn <74682394+yucelenn@users.noreply.github.com>
2022-10-10 18:40:32 +0300

    day 1
    1. ders ödevleri

---
+++ Day_1/07_dataTypes/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int sayi = 12;
+        double sayi2 = 12.5;
+        sayi2 = -129; // double veri tipi int sayıları da kapsar.
+        char karakter = 'A';
+        String kelime = "ANKARA";
+        boolean dogruMu = false; // true false 
+         
+    }
+    
+}
