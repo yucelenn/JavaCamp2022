e6ee1937feda263ac9cc23d6bed8a6393f8ee7fe
yucelenn <74682394+yucelenn@users.noreply.github.com>
2022-10-13 16:46:02 +0300

    day 2 - homework 2
    kodlama.io 2022 JavaCamp ödevleri

---
+++ Day_2/25_methods2/src/Methods2.java
@@ -0,0 +1,31 @@
+public class Methods2 {
+
+    public static void main(String[] args) {
+        String mesaj = "Bugün hava çok güzel.";
+        String yeniMesaj = sehirVer();
+        System.out.println(yeniMesaj);
+        int sayi = topla(5 , 7);
+        System.out.println(sayi);
+    }
+    
+    public static void ekle(){
+        System.out.println("Eklendi.");
+    }
+    
+    public static void sil(){
+        System.out.println("Silindi.");
+    }
+    
+    public static void guncelle(){
+        System.out.println("Güncelendi.");
+    } 
+    
+    public static int topla(int a , int b){
+        return a + b;
+    }
+    
+    public static String sehirVer(){
+        return "Ankara";
+    }
+    
+}
