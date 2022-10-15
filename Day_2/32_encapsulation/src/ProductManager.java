ec41d9ec77fcb622897f0f5757871d79092853e9
yucelenn <74682394+yucelenn@users.noreply.github.com>
2022-10-15 13:46:31 +0300

    day 2 - hw 2
    kodlama.io 2022 Java Yazılım Geliştirici Yetiştirme Kampı 2. gün 2. ödev

---
+++ Day_2/32_encapsulation/src/ProductManager.java
@@ -0,0 +1,10 @@
+public class ProductManager {
+    
+    public void add(Product product){
+        //JDBC
+        //System.out.println(product.name + " Ürün eklendi."); 
+        //private çevirdiğimiz için(field) product.name i kullanamayız
+        
+    }
+    
+}
