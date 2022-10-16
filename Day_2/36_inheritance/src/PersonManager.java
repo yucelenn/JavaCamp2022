b78a64d2966da69f1476d0a9e1dc55ef55d31658
yucelenn <74682394+yucelenn@users.noreply.github.com>
2022-10-16 17:16:31 +0300

    day 2 - hw 2 - video 36
    kodlama.io 2022 JavaCamp 2. gün 2. ödev

---
+++ Day_2/36_inheritance/src/PersonManager.java
@@ -0,0 +1,11 @@
+public class PersonManager {
+    
+    public void list(){
+        System.out.println("müşteri listelendi.");
+    }
+    
+    public void add(){
+        System.out.println("müşteri eklendi.");
+    }
+    
+}
