8b1cbcf65e4ad39bb1144244c6e65ea2528278e4
yucelenn <74682394+yucelenn@users.noreply.github.com>
2022-10-19 16:37:11 +0300

    day 3 - hw2 - video 33
    kodlama.io 2022 Java  Yazılım Geliştirici Yetiştirme Kampı 3. gün 2. ödev

---
+++ Day_3/33_getterSetter/src/ProductManager.java
@@ -0,0 +1,9 @@
+public class ProductManager {
+    
+    public void add(Product product){
+        //JDBC
+        System.out.println(product.getName() + " Ürün eklendi."); 
+        
+    }
+    
+}
